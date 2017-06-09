1b8416479e3523bb99767eca8ad4f347b3589967
Samuel Ruiz <samuelpuckett77@gmail.com>
2017-06-09 15:54:20 -0700

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  Simple Calculator
+//
+//  Created by Samuel Ruiz on 6/1/17.
+//  Copyright © 2017 Samuel Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstNumberTxt: UITextField!
+    @IBOutlet weak var secondNumberTxt: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    resultLabel.text = "Result is: "
+    
+    
+        
+    }
+    
+    @IBAction func plusButton(_ sender: Any) {
+        if let firstNumber = Int(firstNumberTxt.text!) {
+            if let secondNumber = Int(secondNumberTxt.text!) {
+                result = firstNumber + secondNumber
+                resultLabel.text = "Result is: \(String(result))"
+            }
+        }
+    }
+    
+    @IBAction func minusButton(_ sender: Any) {
+        if let firstNumber = Int(firstNumberTxt.text!) {
+            if let secondNumber = Int(secondNumberTxt.text!) {
+                result = firstNumber - secondNumber
+                resultLabel.text = "Result is: \(String(result))"
+            }
+        }
+    }
+    
+    @IBAction func multiplyButton(_ sender: Any) {
+        if let firstNumber = Int(firstNumberTxt.text!) {
+            if let secondNumber = Int(secondNumberTxt.text!) {
+                result = firstNumber * secondNumber
+                resultLabel.text = "Result is: \(String(result))"
+            }
+        }
+    }
+
+    @IBAction func divideButton(_ sender: Any) {
+        if let firstNumber = Int(firstNumberTxt.text!) {
+            if let secondNumber = Int(secondNumberTxt.text!) {
+                result = firstNumber / secondNumber
+                resultLabel.text = "Result is: \(String(result))"
+            }
+        }
+    }
+    
+    @IBAction func remainderButton(_ sender: Any) {
+        if let firstNumber = Int(firstNumberTxt.text!) {
+            if let secondNumber = Int(secondNumberTxt.text!) {
+                result = firstNumber % secondNumber
+                resultLabel.text = "Result is: \(String(result))"
+            }
+        }
+    }
+    
+
+}
+
